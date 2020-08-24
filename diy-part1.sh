68edf1513026a5f21b25c59d624ccfae5a8a7af9
sayhello023 <61860033+sayhello023@users.noreply.github.com>
2020-08-24 09:27:02 +0800

    update

---
+++ diy-part1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git mypackages https://github.com/siropboy/mypackages' feeds.conf.default
+sed -i '$a src-git maxminddb https://github.com/jerrykuku/lua-maxminddb' feeds.conf.default
+sed -i '$a src-git vssr https://github.com/jerrykuku/luci-app-vssr' feeds.conf.default
