7c332deb2d4c10488e583e3a4ef0c731b0c436d6
sayhello023 <61860033+sayhello023@users.noreply.github.com>
2020-09-02 19:14:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 git clone -b master https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 git clone -b master https://github.com/sayhello023/luci-app-vssr package/luci-app-vssr
-git clone -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon-18.06
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon-18.06
