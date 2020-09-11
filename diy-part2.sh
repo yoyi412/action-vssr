80b61af9edc2a3e276da5833d1b40c32d3180b17
sayhello023 <61860033+sayhello023@users.noreply.github.com>
2020-09-11 11:50:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.5/g' package/base-files/files/bin/config_generate
 git clone -b master https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 git clone -b master https://github.com/sayhello023/luci-app-vssr package/luci-app-vssr
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon-18.06
