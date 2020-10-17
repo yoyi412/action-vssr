296d7e0b3281386e72d8e5e5785e8e3cd5b5e0ca
yoyi412 <43584510+yoyi412@users.noreply.github.com>
2020-10-17 09:53:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,11 +9,11 @@
 #=============================================================
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git mypackages https://github.com/siropboy/mypackages' feeds.conf.default
 #sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-#sed -i '$a src-git jerryk thttps://github.com/fw876/helloworld' feeds.conf.defaul
+#sed -i '$a src-git helloworld thttps://github.com/fw876/helloworld' feeds.conf.defaul
