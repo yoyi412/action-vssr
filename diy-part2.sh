36d142da167e91582b643833c7fb8d960cdd1189
yoyi412 <43584510+yoyi412@users.noreply.github.com>
2020-10-17 15:04:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,3 +16,13 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-th
 git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone -b master https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
 #git clone -b master https://github.com/fw876/helloworld package/luci-app-ssr-plus
+
+# 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='LEUNG'' package/lean/default-settings/files/zzz-default-settings
+
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
+# 修改内核版本
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' target/linux/x86/Makefile
