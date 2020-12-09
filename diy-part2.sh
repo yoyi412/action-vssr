c6d6dd197c307ea2dd52c777ebfc1c6391502517
yoyi412 <43584510+yoyi412@users.noreply.github.com>
2020-12-09 17:35:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,19 +10,19 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-git clone -b master https://github.com/jerrykuku/lua-maxminddb package/lean/lua-maxminddb
-git clone -b master https://github.com/jerrykuku/luci-app-vssr package/lean/luci-app-vssr
-git clone -b main https://github.com/XTLS/Xray-core package/lean/Xray-main
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon-18.06
-git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
-git clone -b master https://github.com/jerrykuku/luci-app-jd-dailybonus package/lean/luci-app-jd-dailybonus
-git clone https://github.com/jerrykuku/node-request.git package/lean/node-request  #京东签到依赖
-git clone -b master https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash  #openclash出国软件
-git clone https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash  #clash出国软件
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns  #smartdns DNS加速
-git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos  #内网IP限速工具
-svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/lean/luci-app-passwall  #passwall出国软件
-svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lean/package  #passwall出国软件配套
+git clone -b master https://github.com/jerrykuku/lua-maxminddb package/leung/lua-maxminddb
+git clone -b master https://github.com/jerrykuku/luci-app-vssr package/leung/luci-app-vssr
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/leung/luci-theme-argon-18.06
+git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/leung/luci-app-argon-config
+git clone -b master https://github.com/jerrykuku/luci-app-jd-dailybonus package/leung/luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/node-request.git package/leung/node-request  #京东签到依赖
+git clone -b master https://github.com/vernesong/OpenClash.git package/leung/luci-app-openclash  #openclash出国软件
+git clone https://github.com/frainzy1477/luci-app-clash.git package/leung/luci-app-clash  #clash出国软件
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/leung/luci-app-smartdns  #smartdns DNS加速
+git clone https://github.com/garypang13/luci-app-eqos.git package/leung/luci-app-eqos  #内网IP限速工具
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/leung/openwrt-passwall
+svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/leung/luci-app-passwall  #passwall出国软件
+svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/leung/package  #passwall出国软件配套
 #git clone -b master https://github.com/fw876/helloworld package/luci-app-ssr-plus
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
